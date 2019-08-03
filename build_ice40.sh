0e1f904a96079e730fc3e55a798e4c34832ac598
Franz Flasch <franz.flasch@gmx.at>
2019-08-03 20:33:29 +0200

    Some adaptions for synthesis

--- build_ice40.sh
+++ build_ice40.sh
@@ -2,5 +2,6 @@
 
 set -e
 
-yosys -p 'synth_ice40 -top top -json leiwand_rv32_soc_hx8k.json' helper.v leiwand_rv32_constants.v leiwand_rv32_core.v leiwand_rv32_ram.v leiwand_rv32_soc_hx8k.v
+yosys -p 'synth_ice40 -top leiwandrv32_soc_hx8k -json leiwand_rv32_soc_hx8k.json' helper.v leiwand_rv32_constants.v leiwand_rv32_core.v leiwand_rv32_ram.v leiwand_rv32_soc_hx8k.v
 nextpnr-ice40 --hx8k --json leiwand_rv32_soc_hx8k.json --pcf ice40hx8k-evb.pcf --asc leiwand_rv32_soc_hx8k.asc 
+icepack leiwand_rv32_soc_hx8k.asc leiwand_rv32_soc_hx8k.bin  
--- leiwand_rv32_soc_hx8k.v
+++ leiwand_rv32_soc_hx8k.v
@@ -1,19 +1,89 @@
+`timescale 1ns/1ps 
+
 `include "helper.v"
 `include "leiwand_rv32_constants.v"
 
-module top(						//top module
+//`define TESTBENCH_MODE
+
+
+module clk_divn #(
+    parameter WIDTH = 3,
+    parameter N = 5)
+    (clk, clk_out);
+
+    input clk;
+    output clk_out;
+
+    reg [WIDTH-1:0] pos_count = 0;
+    reg [WIDTH-1:0] neg_count = 0;
+    wire [WIDTH-1:0] r_nxt;
+
+    always @(posedge clk)
+    if (pos_count == N-1) pos_count <= 0;
+    else pos_count <= pos_count +1;
+
+    always @(negedge clk)
+    if (neg_count ==N-1) neg_count <= 0;
+    else neg_count<= neg_count +1;
+
+    assign clk_out = ((pos_count > (N>>1)) | (neg_count > (N>>1)));
+endmodule
+
+`ifdef TESTBENCH_MODE
+module leiwandrv32_soc_hx8k();
+`else
+module leiwandrv32_soc_hx8k(
 	CLK,
 	RST,
     LED1,
     LED2
 );
+`endif
+    parameter MEMORY_SIZE = 8;
 
-    parameter MEMORY_SIZE = 1024;
+`ifdef TESTBENCH_MODE
+    reg CLK = 0;
+    reg RST = 0;
+    wire LED1;
+    wire LED2;
 
+    initial begin 
+        CLK=0;
+        forever #2 CLK=~CLK;
+    end
+
+    initial begin
+        #5
+        RST=1;
+        #50
+        RST=0;
+        #50
+        RST=1;
+    end
+
+    initial begin
+        $dumpfile("leiwand_rv32_soc_hx8k_tb.vcd");
+        $dumpvars(0, leiwandrv32_soc_hx8k);
+
+        // for (i = 0; i < `NR_RV_REGS; i = i + 1) begin
+        //     $dumpvars(0, cpu_core.x[i]);
+        // end
+
+        // for (i = 0; i < MEMORY_SIZE; i = i + 1) begin
+        //     $dumpvars(0, internal_rom.mem[i]);
+        // end
+
+        # 150000 $finish;
+    end
+
+`else
     input CLK;
     input RST;
-    output	LED1;				//output signal to LED1
-    output	LED2;				//output signal to LED2
+    output LED1;
+    output LED2;
+`endif
+
+    wire system_clock;
 
     wire wb_ack;
     wire [(`MEM_WIDTH-1):0] wb_data_in;
@@ -30,10 +100,12 @@ module top(						//top module
     wire wb_ack_rom;
     wire wb_stall_rom;
 
+    clk_divn #(.WIDTH(32), .N(8)) slow_clk(CLK, system_clock);
+
     leiwand_rv32_core
         cpu_core (
-            CLK, 
-            RST,
+            system_clock, 
+            !RST,
 
             wb_ack,
             wb_data_in,
@@ -43,15 +115,17 @@ module top(						//top module
             wb_cyc,
             wb_addr,
             wb_data_out,
-            data_write_size
+            data_write_size,
+            LED1
     );
 
     leiwand_rv32_ram #(
         .MEM_WIDTH(`MEM_WIDTH),
         .MEM_SIZE(MEMORY_SIZE)
     ) internal_rom (
-        clk,
-        reset,
+        system_clock,
+        !RST,
+
         wb_addr,
         wb_data_out,
         wb_data_in_rom,
@@ -63,12 +137,12 @@ module top(						//top module
         data_write_size
     );
 
-    assign wb_stb_internal_rom = wb_stb && ( (wb_addr >= `MEM_WIDTH'h20400000) && (wb_addr < `MEM_WIDTH'h20400000 + (4*MEMORY_SIZE)) );
+    assign wb_stb_internal_rom = wb_stb & wb_addr[22];// wb_stb && ( (wb_addr >= `MEM_WIDTH'h20400000) && (wb_addr < `MEM_WIDTH'h20400000 + (4*MEMORY_SIZE)) );
     assign wb_data_in = wb_data_in_rom;
     assign wb_stall = wb_stall_rom;
     assign wb_ack = wb_ack_rom;
 
-    assign LED1 = wb_cyc;
-    assign LED2 = wb_stb_internal_rom;
+    //assign LED1 = wb_cyc; //cpu_core.x[10][0];
+    assign LED2 = wb_stb; //cpu_core.x[11][0];
 
 endmodule
