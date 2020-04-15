dc41c1298d4c397f8522b50f37bc9db195f1bb84
Franz Flasch <franz.flasch@gmx.at>
2020-04-15 10:09:25 +0000

    Minor refactoring

--- isa_test/build_leiwandrv32_tests_verilator.sh
+++ isa_test/build_leiwandrv32_tests_verilator.sh
@@ -9,71 +9,7 @@ fi
 
 ARCH="$1"
 
-tests=(
-lui
-auipc
-jal
-jalr
-beq
-bne
-blt
-bge
-bltu
-bgeu
-lb
-lh
-lw
-lbu
-lhu
-sb
-sh
-sw
-addi
-slti
-sltiu
-xori
-ori
-andi
-slli
-srli
-srai
-add
-sub
-sll
-slt
-sltu
-xor
-srl
-sra
-or
-and
-# #fence
-# #fence_i
-# #ecall
-# #ebreak
-# #csrrw
-# #csrrs
-# #csrrc
-# #csrrwi
-# #csrrsi
-# #csrrci
-)
-
-if [ "${ARCH}" = "64" ]; then
-    tests+=(
-        addiw
-        slliw
-        srliw
-        sraiw
-        addw
-        subw
-        sllw
-        srlw
-        sraw
-        ld
-        lwu
-    )
-fi;
+source "${BASH_SOURCE%/*}/tests.sh"
 
 mkdir -p leiwandrv32_compiled_files
 mkdir -p leiwandrv32_traces
